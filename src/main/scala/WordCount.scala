a6816ff5e05fbe04db2afe9f80c3472f9e8ab24c
1Gize <grzolko@gmail.com>
2022-04-26 12:59:03 +0200

    build change

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,17 +1,23 @@
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
+import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig, scala}
 import org.apache.kafka.streams.kstream.{KStream, Printed}
+import org.apache.kafka.streams.scala._
 
 import java.util
 import java.util.Properties
-
+import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.serialization.Serdes._
 object WordCount extends App{
-  val ksb = new StreamsBuilder
-  val input: KStream[String, String] = ksb.stream("ksWordCount")
-  val wordCount = input.
-    flatMapValues(line =>  util.Arrays.asList(line.split("\\W+")))
-    .groupBy((key,w) => w)
-    .count()
+  val ksb = new scala.StreamsBuilder()
+  val input = ksb.stream[String,String]("ksWordCount")
+     input
+    .flatMapValues{_.split("\\W+")}
+    .groupBy{(_,w) => w}
+    .count
+    .mapValues(_.toString)
+       .toStream
+    .to("output1")
   input.print(Printed.toSysOut[String,String].withLabel("[What]"))
   val topology = ksb.build()
   println(topology.describe())
@@ -20,6 +26,7 @@ object WordCount extends App{
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,":9092")
   props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
   props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-  private val ks = new KafkaStreams(topology,props)
+  val streamCfg = new StreamsConfig(props)
+  val ks = new KafkaStreams(topology,streamCfg)
   ks.start()
 }
